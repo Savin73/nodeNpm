7f16675dfcdf92880fb77b175769edc7a2f8d321
Savin73 <savin73@gmail.com>
2025-01-13 23:46:13 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-import(add, subtract); from `./math.js`;
+import { add, subtract } from './math.js';
+
+let a = 20,
+    b = 5;
+
+let result = add(a, b);
+console.log(`${a} + ${b} = ${result}`);
+
+result = subtract(a, b);
+console.log(`${a} - ${b} = ${result}`);
